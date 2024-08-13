9aa807b5dd2920595b5d78bd2711a76cf229835f
Alluri Harsha Sri <146983301+alluriharshasri@users.noreply.github.com>
2024-08-13 13:13:48 +0530

    Add files via upload

---
+++ SelectionSort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void SelectionSort(int x[], int n){
+    int Temp;
+    for (int i = 0; i < n-2 ; i++){                   // i <= n-2
+        for (int j = 0 ; j < n-1-i ; j++){            // j < n-1-i
+            if(x[j]>x[j+1]){
+                Temp = x[j];
+                x[j] = x[j+1];
+                x[j+1] = Temp;
+            }
+        }
+    }
+}
+
+void Display(int x[], int n){
+    for (int i = 0; i < n; i++){
+        printf("%d \t",x[i]);
+    }
+}
+
+int main(){
+    int n,a[10];
+    printf("Enter Number Of Elements You Want To Store : ");
+    scanf("%d",&n);
+    for (int i = 0; i < n ; i++){
+        printf("Enter Element %d : ",i+1);
+        scanf("%d",&a[i]);
+    }
+    SelectionSort(a,n);
+    Display(a,n);
+    return 0;
+}
